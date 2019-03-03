8da33eceae2fa7d0996bfb101742694ea28858fc
johnale <johnale@cisco.com>
2019-03-03 16:42:10 -0500

    added add to inventory and add new product

--- bamazon.js
+++ bamazon.js
@@ -29,9 +29,48 @@ const getLowInventory = () => {
     });
 };
 
+const addProduct = () => {
+    inquirer.prompt([{
+        type: 'input',
+        message: 'Enter Product Name: ',
+        name: 'product_name'
+    },{
+        type: 'input',
+        message: 'Enter Department Name: ',
+        name: 'department_name'
+    },{
+        type: 'input',
+        message: 'Enter Product Price: ',
+        name: 'price'
+    },{
+        type: 'input',
+        message: 'Enter Product Quantity: ',
+        name: 'stock_quantity'
+    }]).then(newProduct => {
+        const sql = `INSERT INTO products SET ?`;
+        db.query(sql, newProduct, (error, results) => {
+            if(error) throw error;
+            console.log(`New Product Added Successfully`);
+        });
+    });
+};
+
+const addInventory = () => {
+    const sql = `SELECT * FROM products`;
+    return new Promise((resolve, reject) => {
+        db.query(sql, (error, Results) => {
+            if (error) throw error;
+            resolve(Results);
+        });
+    })
+
+};
+
 module.exports = {
     getCatalog: getCatalog,
-    getLowInventory: getLowInventory
+    getLowInventory: getLowInventory,
+    addProduct: addProduct,
+    addInventory: addInventory
 };
 
 
--- bamazonManager.js
+++ bamazonManager.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 const db = require('./database');
-const {getCatalog, getLowInventory} = require('./bamazon');
+const {getCatalog, getLowInventory, addProduct, addInventory} = require('./bamazon');
 
 /*
   * List a set of menu options:
@@ -30,8 +30,37 @@ const start = () => {
                 getLowInventory();
                 start();
                 break;
+            case 'Add to Inventory':
+                addInventory().then(items => {
+                    inquirer.prompt([{
+                        type: 'rawlist',
+                        message: 'Select an item to add more quantity.',
+                        choices: items.map(item => item.product_name),
+                        name: 'inventory'
+                    }]).then(selection => {
+                        inquirer.prompt([{
+                            type: 'input',
+                            message: 'Quantity number to add: ',
+                            name: 'amount'
+                        }]).then(addedQuantity => {
+                            const itemAmount = items.filter(item => item.product_name === selection.inventory);
+                            const sql = `UPDATE products SET stock_quantity=${parseInt(itemAmount[0].stock_quantity) + parseInt(addedQuantity.amount)} WHERE product_name="${selection.inventory}"`
+                            db.query(sql, (error, updateResults) => {
+                                if (error) throw error;
+                                console.log(`Added Quantity sucessfully`);
+                                start();
+                            })
+                        })
+                    });
+                });
+                // start();
+                break;
+            case 'Add New Product':
+                addProduct();
+                start();
+                break;
             default:
-                console.log('hey');
+                start();
                 break;
         }
     });
