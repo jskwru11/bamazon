4c23403accc783d963b0756a618801f22f7984a3
johnale <johnale@cisco.com>
2019-03-11 00:10:39 -0400

    added getCatalog to customer

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,11 +1,13 @@
 const inquirer = require('inquirer');
 const db = require('./database');
+const {getCatalog} = require('./bamazon');
 
 
 
 //    * The first should ask them the ID of the product they would like to buy.
 //    * The second message should ask how many units of the product they would like to buy.
 const start = () => {
+    getCatalog();
     inquirer.prompt([{
         type: 'input',
         message: 'What is the ID of the product you would like to buy?',
